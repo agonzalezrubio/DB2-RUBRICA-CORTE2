64bef072a563ddbce1da989092ece3bd7fd06ac1
Alejandro <alejandro@urspos.com>
2023-04-27 22:17:31 -0500

    Port fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import roomRoutes from './routes/room.routes.js';
 import bookingRoutes from './routes/booking.routes.js';
 
-import { APP_PORT } from './config.js';
+import { PORT } from './config.js';
 
 const app = express();
 app.use(express.json())
@@ -11,5 +11,5 @@ app.use(roomRoutes);
 app.use(bookingRoutes);
 
 app.listen(APP_PORT, () => {
-  console.log(`App iniciada en el puerto ${APP_PORT}`);
+  console.log(`App iniciada en el puerto ${PORT}`);
 });
